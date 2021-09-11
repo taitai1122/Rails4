2e34ce2ebe352f1a46455248192b2b113ed1f499
EC2 Default User <ec2-user@ip-172-31-0-234.ec2.internal>
2021-09-11 09:52:57 +0000

    Initial commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,19 +6,32 @@ class TodolistsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to todolist_path(list.id)
+     @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
   end
 
 
-  
+
   def index
     @lists = List.all
   end
 
   def show
     @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
 
